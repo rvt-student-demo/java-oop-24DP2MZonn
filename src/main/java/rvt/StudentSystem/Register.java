84ed2a0a2de884feb866c10c32038bc67015d738
Martiņš <A240677MZ@rvt.lv>
2026-02-18 15:37:00 +0200

    Nepabeigts Studentu registrs

--- src/main/java/rvt/StudentSystem/Register.java
+++ src/main/java/rvt/StudentSystem/Register.java
@@ -2,8 +2,8 @@ package rvt.StudentSystem;
     import java.util.ArrayList;
     import java.util.List;
 public class Register {
-    private String name;
-    private String surname;
+    private String name = "^[A-Z][a-z]+(?: [A-Z][a-z]+)*$";
+    private String surname = "^[A-Z][a-z]+(?: [A-Z][a-z]+)*$";
     private String email;
     private String personaskods;
     List<String> registerlists;
@@ -18,5 +18,21 @@ public class Register {
 
     public void add(String registerlist){
         registerlists.add(registerlist);
+    }
+    public void print(){
+        for(int i = 0; i < registerlists.size(); i++  ){
+            System.out.println(i + 1 + ": " + registerlists.get(i));
+        }
+    }
+    public void remove(int number){
+        int i = number - 1;
+        if(i >= 0){
+            if(i < registerlists.size()){
+                registerlists.remove(i);
+            }else{
+                System.out.println("Error");
+            }
+            
+        }
     }
 }
