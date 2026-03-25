083de7cd8a10ae95885ad721eadb947e6dc3d1a2
Martiņš <A240677MZ@rvt.lv>
2026-03-25 14:13:08 +0200

    Nepabeigts online shop

--- src/main/java/rvt/Shop/OnlineShop.java
+++ src/main/java/rvt/Shop/OnlineShop.java
@@ -2,6 +2,7 @@ package rvt.Shop;
     import java.util.Map;
     import java.util.Set;
     import java.util.HashMap;
+    import java.util.Scanner;
 public class OnlineShop {
     public class Warehouse {
         private Map<String, Integer> collection = new HashMap<>();
@@ -82,6 +83,45 @@ public class OnlineShop {
             }
         }
         
+    }
+    public class Store {
+
+        private Warehouse warehouse;
+        private Scanner scanner;
+    
+        public Store(Warehouse warehouse, Scanner scanner) {
+            this.warehouse = warehouse;
+            this.scanner = scanner;
+        }
+    
+        // the method that handles the customers visit to the store.
+        public void shop(String customer) {
+            ShoppingCart cart = new ShoppingCart();
+            System.out.println("Welcome to the store " + customer);
+            System.out.println("our selection:");
+    
+            for (String product : this.warehouse.products()) {
+                System.out.println(product);
+            }
+    
+            while (true) {
+                System.out.print("What to put in the cart (press enter to go to the register): ");
+                String product = scanner.nextLine();
+                if (product.isEmpty()) {
+                    break;
+                } else {
+                    
+                }
+    
+                // Add code here that adds the product to the cart,
+                // If there is any in the warehouse, and reduces the stock in the warehouse
+                // Don't touch any of the other code!
+            }
+    
+            System.out.println("your shoppingcart contents:");
+            cart.print();
+            System.out.println("total: " + cart.price());
+        }
     }
     public static void main(String[] args) {
         OnlineShop shop = new OnlineShop();
